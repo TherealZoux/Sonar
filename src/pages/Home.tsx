b87048bd4f8f9d9838238315454b9dc0ead33530
therealzoux <itszoux@gmail.com>
2026-06-06 02:28:20 +0300

    feat: change time

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -16,7 +16,7 @@ export default function Home() {
 
     const timer = setInterval(() => {
       setActiveIndex((prev) => (prev + 1) % topCharts.length);
-    }, 10000)
+    }, 5000)
 
     return () => clearInterval(timer);
   }, [topCharts.length]);
